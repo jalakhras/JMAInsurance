9b5154ce99311e4ff4cf42e92fc81464dbcf55f6
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-04-12 23:15:15 +0300

    Implementing Cross Property Validation For PrimaryEmployer and  PreviousEmployer

--- JMAInsurance/JMAInsurance.Models/ViewModel/EmploymentVM.cs
+++ JMAInsurance/JMAInsurance.Models/ViewModel/EmploymentVM.cs
@@ -5,17 +5,12 @@ namespace JMAInsurance.Models.ViewModel
 {
     public class EmploymentVM
     {
-        [Required]
         [Display(Name = "Employment Type")]
         public string EmploymentType { get; set; }
-        [Required]
         public string Employer { get; set; }
-        [Required]
         public string Position { get; set; }
-        [Required]
         [Display(Name = "Gross Monthly Income")]
-        public double GrossMonthlyIncome { get; set; }
-        [Required]
+        public double? GrossMonthlyIncome { get; set; }
         [Display(Name = "Start Date")]
         public DateTime? StartDate { get; set; }
     }
--- JMAInsurance/JMAInsurance.Models/ViewModel/Employments.cs
+++ JMAInsurance/JMAInsurance.Models/ViewModel/Employments.cs
@@ -1,6 +1,10 @@
-﻿namespace JMAInsurance.Models.ViewModel
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JMAInsurance.Models.ViewModel
 {
-    public class Employments
+    public class Employments : IValidatableObject
     {
         public Employments()
         {
@@ -9,5 +13,28 @@
         }
         public EmploymentVM PrimaryEmployer { get; set; }
         public EmploymentVM PreviousEmployer { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            if (PrimaryEmployer.StartDate > DateTime.Now.AddYears(-3))
+            {
+                if (PreviousEmployer.EmploymentType != "Unemployed")
+                {
+                    if (string.IsNullOrEmpty(PreviousEmployer.Employer))
+                    {
+                        results.Add(new ValidationResult("Previous employer is required."));
+                    }
+
+                    if (string.IsNullOrEmpty(PreviousEmployer.Position))
+                    {
+                        results.Add(new ValidationResult("Previous position is required."));
+                    }
+                }
+            }
+
+            return results;
+        }
     }
 }
